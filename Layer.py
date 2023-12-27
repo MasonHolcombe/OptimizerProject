82225eb92623c324d825250d4e9281cfc2eff944
MasonHolcombe <mason101h@gmail.com>
2023-12-27 15:22:59 -0600

    Implemented Compile Method

--- Layer.py
+++ Layer.py
@@ -1,9 +1,83 @@
-from Activations import BaseActivation
 import numpy as np
 
-class Layer:
+class BaseLayer:
 
-    def __init__(self, n_nodes:int, activation:BaseActivation):
+    def __init__(self, n_nodes:int):
         self.n_nodes = n_nodes
-        self.activation = activation
+
+    def forward(self, data):
+        pass
+    
+    def backward(self, dx):
+        pass
+
+class Dense(BaseLayer):
+
+    def __init__(self, n_nodes:int, lr_rate):
+        super().__init__(n_nodes)
+        self.weights = list()
+        self.bias = list()
+        self.lr_rate = lr_rate
+    
+    def forward(self, data):
+        self.data = data
+        # if self.weights == [] or self.bias == []:
+        #     raise Exception('Weights and/or Bias not initialized')
+        return np.dot(data, self.weights) + self.bias
+
+    def backward(self, dx):
+        dw = np.dot(self.data.T, dx)
+        db = np.sum(dx, axis=0)
+        self.weights = self.weights - np.dot(self.lr_rate, dw)
+        self.bias = self.bias - np.dot(self.lr_rate, db)
+        res = np.dot(dx, self.weights.T)
+        return res
+
+
+class BaseActivation():
+    # do something
+    def forward(self, vec):
+        pass
+    def backward(self, vec):
+        pass
+
+class ReLU(BaseActivation):
+    def __init__(self):
+        pass
+    def forward(self, vec):
+        self.out = np.maximum(np.zeros(shape=vec.shape), vec)
+        return self.out
+    
+    def backward(self, dx):
+        return dx * np.greater(self.out, np.zeros(shape=self.out.shape)).astype(int)
+    
+class Sigmoid(BaseActivation):
+    def __init__(self):
+        pass
+    def forward(self, vec):
+        self.out = 1 / (1 + np.exp(-vec))
+        return self.out
+    def backward(self, dx):
+        res = dx * self.out * (1 - self.out)
+        return res
+    
+class Linear(BaseActivation):
+    def __init__(self):
+        pass
+    def forward(self, vec):
+        self.out = vec
+        return self.out
+    def backward(self, dx):
+        return dx * np.ones(shape=self.out.shape)
+    
+class Softmax(BaseActivation):
+    def __init__(self):
+        pass
+    def forward(self, vec):
+        exps = np.exp(vec)
+        return exps / np.sum(exps)
+    def backward(self, vec):
+        pass
+
+
 
--- Network.py
+++ Network.py
@@ -1,35 +1,60 @@
 import numpy as np
-from Layer import Layer
+from Layer import BaseActivation, BaseLayer
 
 class Network:
     
     def __init__(self, data):
         self.layers = list()
         self.data = data
-        self.weights = list()
-        self.bias = list()
+        # self.weights = list()
+        # self.bias = list()
 
-    def add(self, layer):
+    def add(self, layer, activation:BaseActivation = None):
+        self.layer = layer
         self.layers.append(layer)
-        self.add_weights()
-        self.add_bias()
+        # print(f'Layer: {type(layer)}')
+        # self.add_weights()
+        # print(f'Weights: {self.layer.weights}')
+        # self.add_bias()
+        # print(f'Bias: {self.layer.bias}')
+        if activation != None:
+            self.layers.append(activation)
 
-    def add_weights(self):
-        if len(self.layers) <= 1:
-            return
-        new_weights = np.random.normal(loc=0, scale=1, size=(self.layers[-2].n_nodes, self.layers[-1].n_nodes))
-        self.weights.append(new_weights)
+    # def add_weights(self):
+    #     # if len(self.layers) <= 1:
+    #     #     return
+    #     new_weights = np.random.normal(loc=0, scale=1, size=(self.layers[-1].n_nodes, ))
+    #     self.layer.weights.append(new_weights)
         
-    def add_bias(self):
-        if len(self.layers) <= 1:
-            return
-        new_bias = np.random.normal(loc=0, scale=1, size=(self.layers[-1].n_nodes, ))
-        self.bias.append(new_bias)
+    # def add_bias(self):
+    #     # if len(self.layers) <= 1:
+    #     #     return
+    #     new_bias = np.random.normal(loc=0, scale=1, size=(1))
+    #     self.layer.bias.append(new_bias)
+            
+    def compile(self):
+        i=0
+        while i<=len(self.layers)-1:
+            if isinstance(self.layers[i], BaseLayer):
+                if i==0: #FIRST DENSE LAYER
+                    weights = np.random.normal(loc=0, scale=1, size=(self.layers[i].n_nodes,self.layers[i+2].n_nodes))
+                    self.layers[i].weights.append(weights)
+                    bias = np.random.normal(loc=0, scale=1, size=(1))
+                    self.layers[i].bias.append(bias)
+                elif i+2 <= len(self.layers)-1: #MIDDLE LAYERS
+                    weights = np.random.normal(loc=0, scale=1, size=(self.layers[i].n_nodes,self.layers[i+2].n_nodes))
+                    self.layers[i].weights.append(weights)
+                    bias = np.random.normal(loc=0, scale=1, size=(1))
+                    self.layers[i].bias.append(bias)
+                elif self.layers[i] == self.layers[-2]: #LAST DENSE LAYER
+                    pass
+               
+                print(f'Dense #: {i%2}\n Layer Type: {self.layers[i]}\n, Weights: {self.layers[i].weights}\n,Bias: {self.layers[i].bias}, Activation: {self.layers[i+1]}')
+            i = i+1
 
     def forward(self):
         intermediate = self.data
-        for i, weight in enumerate(self.weights):
-            intermediate = np.dot(intermediate, weight) + self.bias[i]
-            intermediate = self.layers[i+1].activation.activate(intermediate)
+        for layer in self.layers:
+            intermediate = layer.forward(intermediate)
         return intermediate
-        
+
